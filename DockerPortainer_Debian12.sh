f75fddbc3d1780ffe21abd0cbbe39ebfb875aa2e
Liam L <70714060+liamnarvalo@users.noreply.github.com>
2023-12-03 22:56:46 +0100

    Add files via upload

---
+++ DockerPortainer_Debian12.sh
@@ -0,0 +1,21 @@
+# A EXECUTER SOUS ROOT
+
+# Ajout de la clé GPG de Docker :
+apt update
+apt install -y ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Ajout des dépots officiels de Docker pour Debian 12 :
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Installation de Portainer sur le port HTTPS 9443
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
